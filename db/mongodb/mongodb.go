987e0fc29cb1e34bd576eb2a99d8e4c3890c3c65
squishedfox <christopher.schoeder@protonmail.com>
2024-08-15 07:56:29 -0500

    create mongodb package

---
+++ db/mongodb/mongodb.go
@@ -0,0 +1,14 @@
+package mongodb
+
+import (
+	"ggithub.com/squishedfox/webservice-prototype/db"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type resourceManager struct {
+	session mongo.SessionContext
+}
+
+func NewPersonManager(session mongo.SessionContext) db.PersonResourceManager {
+	return &resourceManager{session: session}
+}
